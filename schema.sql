9de1bffbca0213d6db4e5853c0f6f01af5d4ef3c
Victor Miguez <v.miguez2004@gmail.com>
2024-08-26 20:22:34 -0300

    fix: correção de comentário

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE pessoas (
     id SERIAL PRIMARY KEY,
     apelido VARCHAR(32) UNIQUE NOT NULL,
-    nome VARCHAR(100) NOT NULL, -- por padrão, AAAA-MM-DD
-    nascimento DATE NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    nascimento DATE NOT NULL, -- por padrão, AAAA-MM-DD
     stack TEXT[]
 );
